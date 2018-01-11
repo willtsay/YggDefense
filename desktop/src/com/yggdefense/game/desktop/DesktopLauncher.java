e23a808bc346d8be5fedde8b9e1ee96607379c7d
will tsay <willtsay@gmail.com>
2018-01-11 14:18:56 -0800

    Initial commit

---
+++ desktop/src/com/yggdefense/game/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.yggdefense.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.yggdefense.game.YggDefense;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 960;
+		config.height = 540;
+		config.title = "Ygg Defense";
+		new LwjglApplication(new YggDefense(), config);
+	}
+}
